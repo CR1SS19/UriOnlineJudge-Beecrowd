b4cd4e0ce50873a93f01da9cb24ce0f5cee56395
CR1SS19 <cristhiancaua@gmail.com>
2021-12-18 19:51:12 -0300

    codigos C 1014-1021

---
+++ Iniciante/1019.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+
+    scanf("%d", &n);
+    printf("%d:%d:%d\n", n / 60 / 60, n / 60 % 60, n % 60);
+
+    return 0;
+}
---
+++ Iniciante/1020.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i;
+
+    scanf("%d", &i);
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", i / 365, i % 365 / 30, i % 365 % 30);
+
+    return 0;
+}
