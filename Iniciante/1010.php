2e2f21feb51f9fef1c159f0f0b94dfd07be1ed2f
CR1SS19 <cristhiancaua@gmail.com>
2021-12-18 21:00:26 -0300

    solucao do 1010.php

--- Iniciante/1010.php
+++ Iniciante/1010.php
@@ -1,5 +1,16 @@
 <?php
 
-$
+$l1 = explode(" ", readline());
+$l2 = explode(" ", readline());
+
+$a = intval($l1[0]);
+$b = intval($l1[1]);
+$c = doubleval($l1[2]);
+
+$d = intval($l2[0]);
+$e = intval($l2[1]);
+$f = doubleval($l2[2]);
+
+echo "VALOR A PAGAR: R$ " .  number_format(($b * $c) + ($e * $f), 2, '.', '') . "\n";
 
 ?>
