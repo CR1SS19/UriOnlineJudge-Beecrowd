11684be9ce699117700dc066a79cf3257dd57b38
CR1SS19 <cristhiancaua@gmail.com>
2022-01-01 23:33:39 -0300

    Soluções do 1049 em C, PHP e Python

---
+++ Iniciante/1049.php
@@ -0,0 +1,34 @@
+<?php
+
+$x = readline();
+$y = readline();
+$z = readline();
+
+if($x == 'vertebrado' && $y == 'ave' && $z == 'carnivoro'){
+    $a = 'aguia';
+}
+if($x == 'vertebrado' && $y == 'ave' && $z == 'onivoro'){
+    $a = 'pomba';
+}
+if($x == 'vertebrado' && $y == 'mamifero' && $z == 'onivoro'){
+    $a = 'homem';
+}
+if($x == 'vertebrado' && $y == 'mamifero' && $z == 'herbivoro'){
+    $a = 'vaca';
+}
+if($x == 'invertebrado' && $y == 'inseto' && $z == 'hematofago'){
+    $a = 'pulga';
+}
+if($x == 'invertebrado' && $y == 'inseto' && $z == 'herbivoro'){
+    $a = 'lagarta';
+}
+if($x == 'invertebrado' && $y == 'anelideo' && $z == 'hematofago'){
+    $a = 'sanguessuga';
+}
+if($x == 'invertebrado' && $y == 'anelideo' && $z == 'onivoro'){
+    $a = 'minhoca';
+}
+
+echo "$a\n";
+
+?>
