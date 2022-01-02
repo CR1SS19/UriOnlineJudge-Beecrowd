69b50648654fdf60654aa37d9b5d846bf3d4901f
CR1SS19 <cristhiancaua@gmail.com>
2022-01-02 14:42:50 -0300

    Soluções do 1052 em C e PHP

---
+++ Iniciante/1052.php
@@ -0,0 +1,31 @@
+<?php
+
+$x = readline();
+
+if($x == 1){
+    echo "January\n";
+}elseif($x == 2){
+    echo "February\n";
+}elseif($x == 3){
+    echo "March\n";
+}elseif($x == 4){
+    echo "April\n";
+}elseif($x == 5){
+    echo "May\n";
+}elseif($x == 6){
+    echo "June\n";
+}elseif($x == 7){
+    echo "July\n";
+}elseif($x == 8){
+    echo "August\n";
+}elseif($x == 9){
+    echo "September\n";
+}elseif($x == 10){
+    echo "October\n";
+}elseif($x == 11){
+    echo "November\n";
+}elseif($x == 12){
+    echo "December\n";  
+}
+
+?>
